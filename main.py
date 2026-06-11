f9b304ab637eac89ca09de51585fbe697bad8d47
juntaosun <22288197+juntaosun@users.noreply.github.com>
2026-06-11 22:52:38 +0800

    ComeCut - Python 插件扩展范例项目

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class ExamplePlugin:
     
     CATEGORY = "other/example"   # 大类/子类, 例如: audio/asr 或者 audio/tts
     NAME = "示例插件项目"         # 您的插件名称, 显示在插件列表中
-    VERSION = "1.2.0"            # 插件版本号, 例如: "1.2.0"
+    VERSION = "0.0.1"            # 插件版本号, 例如: "1.2.0"
     AUTHOR = "ComeCut"           # 您的插件作者, 作者名称
     GITHUB = None                # 例如: "https://github.com/user/repo"
     
